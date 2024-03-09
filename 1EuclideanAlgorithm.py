9ad9406233c64728488212660011c8e7cc51f23c
Vikas Singh <vikassingh2101@gmail.com>
2024-03-09 12:02:25 +0530

    Euclidean Algorithm

---
+++ 1EuclideanAlgorithm.py
@@ -0,0 +1,15 @@
+print("Enter integers for which gcd is to be computed:")
+x = int(input("Enter first integer:"))
+y = int(input("Enter second integer:"))
+
+if x >= y:
+    a, b = x, y
+else:
+    a, b = y, x
+
+r = a % b
+while r != 0:
+    a, b = b, r
+    r = a % b
+
+print("GCD({},{})={}".format(x,y,b))
